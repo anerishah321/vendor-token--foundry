a5b1087de6846e348e43df0d9a91b9c407441edc
aneri321 <anerishah@Aneris-MacBook-Pro.local>
2023-06-09 15:04:02 +0530

    first commit

--- src/YourToken..sol
+++ src/YourToken..sol
@@ -1,14 +1,11 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.13;  
+// SPDX-License-Identifier: MIT
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
 
-    function increment() public {
-        number++;
+contract YourToken is ERC20 {
+    constructor() ERC20("Gold", "GLD") {
+        _mint(msg.sender , 1000 * 10 ** 18);
     }
 }
