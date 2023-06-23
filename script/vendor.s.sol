0a0141159f3f92f9ddd0b06a4c4ed98601cad021
aneri321 <anerishah@Aneris-MacBook-Pro.local>
2023-06-23 12:05:23 +0530

    first commit

--- script/vendor.s.sol
+++ script/vendor.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import {Vendor} from "../src/Vendor.sol";
+//import "../src/YourToken..sol";
 
 contract Vendordeploy is Script {
-    function setUp() public {}
+    function run() external returns (Vendor) {
+        vm.startBroadcast();
+        Vendor vendor = new Vendor(address(this));
 
-    function run() public {
-        vm.broadcast();
+        vm.stopBroadcast();
+        return vendor;
     }
 }
+//contract address =  0x90193C961A926261B756D1E5bb255e67ff9498A1
